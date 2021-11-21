71cc8aa1044a9bc98bc37b2f564fa691782f18f4
hongryeokyeong <rkhong@blackholic.com>
2021-11-21 17:10:42 +0900

    Add : vue 공부 (라우터,라이프사이클, v-for/v-show/v-model/v-if)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>Welcome to {{title}} </h1>
+    <input type="text" v-model="input1" >
+    <button type="button" @click="getData" >get</button>
+    <button type="button" @click="setData" >set</button>
+
+    <select class="form-select" v-model="name" @change="changeName">
+      <option :key="i" :value="d.v" v-for="(d,i) in options">{{d.t}}</option>
+    </select>
+
+    <table class="table table-bordered" v-if="tableShow ">
+      <tr :key="i" v-for="(d,i) in options">
+        <td>{{d.v}}</td>
+        <td>{{d.t }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return{
+      title :'려경 뷰 공부하기',
+      title2 : '가디',
+      input1 :'abc',
+      options :[
+        {v:"H", t:"hong"},
+        {v:"K", t:"kim"},
+        {v:"A", t:"Ahn"},
+      ],
+      name: "K",
+      tableShow : true,
+    }
   },
+  watch: {
+    input1() {
+      console.log(this.input1)
+    }
+  },
+  methods: {
+     getData() {
+       alert(this.input1);
+     },
+    setData() {
+       this.input1 = '12345'
+    },
+    changeName() {
+       alert(this.name );
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy');
+  },
+  destroyed() {
+    console.log('de stroyed');
+  }
 };
 </script>
