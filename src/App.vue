66bdcb198bdde7f5d7958ba461192d7bcc426fcd
SohyunAhn <sohyun418@gmail.com>
2021-11-21 17:06:32 +0900

    211121

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header/>
+    <div id="content" class="content">
+      <router-view></router-view>
     </div>
-    <router-view />
   </div>
 </template>
+
+<script>
+import Header from "./components/layout/header.vue";
+
+export  default {
+  name: "App",
+  components: {
+    Header,
+  }
+}
+</script>
 
 <style>
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>!{{title2}}!</h1>
+    <h1>!{{title}}!</h1>
+    <input type="text" v-model="input1" />
+    <button type="button" @click="getData">Get</button>
+    <button type="button" @click="setData">Set</button>
+
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <option :key="i" :value="d.v" v-for="(d,i) in options">{{d.t}}</option>
+    </select>
+
+    <table class="table table-bordered" v-if="tableShow">
+      <tr :key="i" v-for="(d,i) in options">
+        <td>{{d.v}}</td>
+        <td>{{d.t}}</td>
+        <td>{{i+1}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      title: "예헷",
+      title2: "Hi",
+      input1: "abc",
+      options: [
+        {v:"S", t:"Seoul"},
+        {v:"J", t:"Jeju"},
+        {v:"B", t:"Busan"},
+      ],
+      region: "J",
+      tableShow: true,
+    };
+  },
+  watch: {
+    input1() {
+      console.log(this.input1);
+    },
+    title() {},
+  },
+  methods: {
+    getData() {
+      alert(this.input1);
+    },
+    setData() {
+      this.input1 = "12345";
+    },
+    changeRegion() {
+      alert(this.region);
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("destroyed");
   },
 };
 </script>
