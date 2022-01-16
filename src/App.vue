897b7c77414eb7324537e670cf327d1edd77a4cf
hongryeokyeong <rkhong@blackholic.com>
2022-01-16 14:20:44 +0900

    Add : vue 공부2_0116

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
     <Header />
-    <div id="content" class="content ">
-      <router-view />
+<!--    <div id="content" class="content ">-->
+<!--      <router-view />-->
+<!--    </div>-->
+    <div>
+      원룸썁
+      <h4>xx 원룸</h4>
+      <p>{{ price1 }} 만원</p>
+    </div>
+    <div>
+      <h4>xx 원룸</h4>
+      <p>{{ price2 }} 만원</p>
     </div>
   </div>
 </template>
 <script>
 import Header from './components/layout/Header'
+// import Axios form
 
 export default {
   name: 'App',
+  data(){
+    return{
+      price1 : 60,
+      price2 : 70,
+    }
+  },
   components: {
     Header,
   }
